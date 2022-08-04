62fac85009872471ada010558b1a6824539e92eb
SINCHIT BATHAM <sinchit417@gmail.com>
2022-08-04 08:27:01 +0530

    Update Treemaptest.java

--- Treemaptest.java
+++ Treemaptest.java
@@ -6,6 +6,7 @@ public class TreemapTest {
       map.put(12,"Ajay");    
       map.put(10,"Pawan");    
       map.put(6,"Rohit");    
+      map.put(2,"Simran");
       System.out.println("Enteries in the treeMap Before Calling the  remove() method");  
       for(Map.Entry m:map.entrySet())  
       {  
@@ -17,5 +18,35 @@ public class TreemapTest {
       {  
           System.out.println(m.getKey()+" "+m.getValue());      
       }  
+      
+      // Checking for the key_element '10'
+        System.out.println("Is the key '10' present?  in the map" );
+        if( map.containsKey(20)
+           {
+              System.out.println(" Yes the Key 20 is present in the map ");
+           }
+           else
+           {
+              System.out.println(" No the Key 20 is not present in the map ");
+            
+           }
+           
+  
+        // Checking for the key_element '25'
+           
+            System.out.println("Is the key '25' present in the map?);
+           
+            if( map.containsKey(25))
+           {
+              System.out.println(" Yes the Key 25 is present in the map ");
+           }
+           else
+           {
+              System.out.println(" No the Key 25 is not present in the map ");
+            
+           }
+           
+       
+        tree_map.containsKey(5));
       }  
 }  
